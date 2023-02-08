dd963c48a1756cce67889f80cc8a5ea78f3a4e69
Hlib Lozhkovyi <hlib.lozhkovyi@gmail.com>
2023-02-08 12:53:01 +0200

    Updated ui lib ver, updated an custom example

--- src/pages/chat/custom.tsx
+++ src/pages/chat/custom.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from 'react'
 import { ChatSidebar, ChatWindow, requireNextAuth } from "@roq/nextjs";
 import { useRouter } from "next/router";
 import AppLayout from "layout/app/app.layout";
@@ -7,10 +8,27 @@ import styles from "pages/chat/chat.module.css";
 function ChatPage() {
   const router = useRouter();
 
+  const navigateToConversationRoute = useCallback((cid: string | null) => {
+    if (!cid) {
+      return;
+    }
+
+    if (router.query.cid === cid) {
+      return;
+    }
+
+    router.push(`/chat/custom?cid=${cid}`, `/chat/custom?cid=${cid}`, { shallow: true })
+  }, [router])
+
+  const generateConversationLink = useCallback(({ id }: { id: string }) => `/chat/${id}`, [])
+
   return (
     <AppLayout>
       <section className={styles.customChat}>
-        <ChatSidebar className={styles.sidebar} />
+        <ChatSidebar
+          onCurrentConversationIdChanged={navigateToConversationRoute} generateConversationLink={generateConversationLink}
+          className={styles.sidebar}
+        />
         <ChatWindow conversationId={router.query.cid as string} />
       </section>
     </AppLayout>
