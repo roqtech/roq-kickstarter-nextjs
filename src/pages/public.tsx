db6cb49a872aeb2c1f58294eec2aee6fdb1e2225
Hlib Lozhkovyi <hlib.lozhkovyi@gmail.com>
2023-02-09 16:18:13 +0200

    Updated roq/nextjs version. Added public page to test the behavior.

---
+++ src/pages/public.tsx
@@ -0,0 +1,23 @@
+import { useSession } from "@roq/nextjs";
+import AppLayout from "layout/app/app.layout";
+
+function PublicPage() {
+  const { session } = useSession()
+
+  return (
+    <AppLayout>
+      <p>
+        This is public page! Accessible by everyone.
+        If you have a don't have session, you still have an access to it.
+      </p>
+      <p>
+        If you don't have the session, ROQ Components should be disbled (in the error state).
+      </p>
+      <code>
+        {JSON.stringify({ session })}
+      </code>
+    </AppLayout>
+  );
+}
+
+export default PublicPage;
