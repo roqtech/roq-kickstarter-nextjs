19a2b98909990f4108310f4712f5e244c0841382
Jasper Bernales <bernalesjasper@gmail.com>
2023-02-21 18:57:07 +0800

    chat tags testing

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -2,13 +2,19 @@ import { Chat, requireNextAuth } from "@roq/nextjs";
 import { useRouter } from "next/router";
 import AppLayout from "layout/app/app.layout";
 import { routes } from "routes";
+import { useState } from "react";
 
 function ChatPage() {
   const router = useRouter();
-
+  const [hotel, setHotel] = useState('Hotel 1')
   return (
     <AppLayout>
-      <Chat />
+      <select className='input' value={hotel} onChange={e => setHotel(e.target.value)}>
+        <option value='Hotel 1'>Hotel 1</option>
+        <option value='Hotel 2'>Hotel 2</option>
+        <option value='Hotel 3'>Hotel 3</option>
+      </select>
+      <Chat tags={[hotel]}/>
     </AppLayout>
   );
 }
