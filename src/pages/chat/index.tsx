2abd51883659b3b0858a07030907e2f726c77dfe
Jasper Bernales <bernalesjasper@gmail.com>
2023-02-27 16:57:21 +0800

    chat tags test groups

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,22 +1,11 @@
-import { Chat, requireNextAuth, useChatTags } from "@roq/nextjs";
-import { useRouter } from "next/router";
+import { ChatSidebar, requireNextAuth } from "@roq/nextjs";
 import AppLayout from "layout/app/app.layout";
 import { routes } from "routes";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function ChatPage() {
-  const router = useRouter();
   const [hotel, setHotel] = useState('Hotel 1')
-  const { setChatTags } = useChatTags();
-
-  useEffect(() => {
-    if (!!hotel) {
-      return setChatTags([hotel])
-    }
-
-    setChatTags(null);
-  }, [hotel])
-
+  const [userGroup, setUserGroup] = useState<string | undefined>()
   return (
     <AppLayout>
       <select className='input' value={hotel} onChange={e => setHotel(e.target.value)}>
@@ -24,7 +13,12 @@ function ChatPage() {
         <option value='Hotel 2'>Hotel 2</option>
         <option value='Hotel 3'>Hotel 3</option>
       </select>
-      <Chat tags={[hotel]} />
+      <select className='input' value={userGroup} onChange={e => setUserGroup(e.target.value)}>
+        <option value="">Select Group</option>
+        <option value='3e9529fd-e435-47f7-83e0-c01788b8f8fe'>Group 1</option>
+        <option value='b93f65ec-5072-4c53-a2db-d22b7398c007'>Group 2</option>
+      </select>
+      <ChatSidebar tags={[hotel]} userListGroupsFilter={[userGroup]} />
     </AppLayout>
   );
 }
