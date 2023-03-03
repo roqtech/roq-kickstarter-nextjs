cb7a5d82feb784ec1a83875b52b18eb2912df022
Vinh Nguyen <vinh_nguyen1211@yahoo.com.vn>
2023-03-03 22:43:43 +0700

    Sync ui-react, tweak user profile

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,11 +1,12 @@
+import styles from "layout/app/app.layout.module.css";
 import AppLayout from "layout/app/app.layout";
-import { UserInvitesTable, requireNextAuth, UserProfileAvatar } from "@roq/nextjs";
+import { requireNextAuth, UserProfileAvatar } from "@roq/nextjs";
 import { routes } from "routes";
 
 function ProfilePage() {
   return (
     <AppLayout>
-      <UserProfileAvatar open withAvatar={false} />
+      <UserProfileAvatar open withAvatar={false} className={styles.userProfile} />
     </AppLayout>
   );
 }
